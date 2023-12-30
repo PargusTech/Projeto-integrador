b113112275adc7e69a6e92d5aa84929ebaf51647
Wytheny <wtmarinho55@gmail.com>
2023-12-30 13:16:29 -0300

    Initial-Page-V1

--- Front-End/InitialPage/script.js
+++ Front-End/InitialPage/script.js
@@ -14,3 +14,16 @@ const btn_close_modal = document.querySelector(".close-modal")
 btn_contact.addEventListener('click', function(){
   view_modal(".modal")
 })
+
+const btn_password = document.querySelector("#btn-view-password")
+
+btn_password.addEventListener('click', function(){
+  let password = document.querySelector("#input-password")
+  if(password.type === "password"){
+    password.type = "text"
+    btn_password.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+  } else{
+    password.type = "password"
+     btn_password.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+  }
+})
