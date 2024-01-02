9555779327e1637bdb68e0fcc2f5c11023a8f931
Wytheny <wtmarinho55@gmail.com>
2024-01-02 20:02:16 -0300

    Login-Page-V1

---
+++ Front-End/LoginPage/script.js
@@ -0,0 +1,12 @@
+const btn_password = document.querySelector("#btn-view-password")
+
+btn_password.addEventListener('click', function(){
+  let password = document.querySelector("#input-password")
+  if(password.type === "password"){
+    password.type = "text"
+    btn_password.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
+  } else{
+    password.type = "password"
+     btn_password.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
+  }
+})
